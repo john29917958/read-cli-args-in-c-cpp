82611bd708b464608eb21069f8f345979134fc19
Tzu-Chao Wang <john29917958@gmail.com>
2024-09-21 08:08:43 +0800

    Encapsulate printing cli args C++ code to a function
    Encapsulates printing command line arguments code into a function for
    C++ program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <string>
 
+void printArgs(int argc, char **argv);
+
 int main(int argc, char **argv)
 {
-    std::cout << "Executable file path is: " << argv[0] << std::endl;
-    std::cout << "Number of arguments is: " << argc << std::endl;
-    for (int i = 1; i < argc; i++)
-    {
-        std::cout << "Argument[" << i << "] is: " << argv[i] << std::endl;
-    }
+    printArgs(argc, argv);
 
     // Read command line arguments into variables.
     std::string execFilePath = argv[0];
@@ -17,4 +14,14 @@ int main(int argc, char **argv)
     double arg3 = std::stod(argv[3]);
 
     return 0;
+}
+
+void printArgs(int argc, char **argv)
+{
+    std::cout << "Executable file path is: " << argv[0] << std::endl;
+    std::cout << "Number of arguments is: " << argc << std::endl;
+    for (int i = 1; i < argc; i++)
+    {
+        std::cout << "Argument[" << i << "] is: " << argv[i] << std::endl;
+    }
 }
