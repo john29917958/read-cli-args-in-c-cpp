74a2bacfabbf9995043d6cb138c1366e9ee72c62
Tzu-Chao Wang <john29917958@gmail.com>
2024-09-21 08:06:17 +0800

    Encapsulate printing cli args code to a function
    Encapsulates printing command line arguments code into a function.

--- src/main.c
+++ src/main.c
@@ -3,14 +3,11 @@
 #include "string.h"
 #include "stdlib.h"
 
+void print_args(int argc, char **argv);
+
 int main(int argc, char **argv)
 {
-    printf("Executable file path is: %s\n", argv[0]);
-    printf("Number of command line arguments is: %d\n", argc);
-    for (int i = 1; i < argc; i++)
-    {
-        printf("Argument[%d] is: %s\n", i, argv[i]);
-    }
+    print_args(argc, argv);
 
     // Read command line arguments into variables.
     char *exec_file_path = argv[0];
@@ -22,4 +19,14 @@ int main(int argc, char **argv)
     sscanf(argv[3], "%lf", &arg3);
 
     return 0;
+}
+
+void print_args(int argc, char **argv)
+{
+    printf("Executable file path is: %s\n", argv[0]);
+    printf("Number of command line arguments is: %d\n", argc);
+    for (int i = 1; i < argc; i++)
+    {
+        printf("Argument[%d] is: %s\n", i, argv[i]);
+    }
 }
